57bc1f652c0c8faa695624f3f8c7e6d25e4c463e
psnx <panyitamas@gmail.com>
2017-09-24 21:18:52 +0200

    Just a test to get the car moving

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -35,16 +35,19 @@ class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
 
-        vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
-        fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
-        brake_deadband = rospy.get_param('~brake_deadband', .1)
-        decel_limit = rospy.get_param('~decel_limit', -5)
-        accel_limit = rospy.get_param('~accel_limit', 1.)
-        wheel_radius = rospy.get_param('~wheel_radius', 0.2413)
-        wheel_base = rospy.get_param('~wheel_base', 2.8498)
-        steer_ratio = rospy.get_param('~steer_ratio', 14.8)
-        max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
-        max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
+        # TODO: Create `TwistController` object
+        self.controller = TwistController(self)
+        
+        controller.vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
+        controller.fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
+        controller.brake_deadband = rospy.get_param('~brake_deadband', .1)
+        controller.decel_limit = rospy.get_param('~decel_limit', -5)
+        controller.accel_limit = rospy.get_param('~accel_limit', 1.)
+        controller.wheel_radius = rospy.get_param('~wheel_radius', 0.2413)
+        controller.wheel_base = rospy.get_param('~wheel_base', 2.8498)
+        controller.steer_ratio = rospy.get_param('~steer_ratio', 14.8)
+        controller.max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
+        controller.max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
 
         self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
                                          SteeringCmd, queue_size=1)
@@ -53,9 +56,7 @@ class DBWNode(object):
         self.brake_pub = rospy.Publisher('/vehicle/brake_cmd',
                                          BrakeCmd, queue_size=1)
 
-        # TODO: Create `TwistController` object
-        self.controller = TwistController(self, vehicle_mass, wheel_radius, brake_deadband,
-                 decel_limit, accel_limit, wheel_base, steer_ratio, max_lat_accel, max_steer_angle)
+        
         
 
 
@@ -63,7 +64,7 @@ class DBWNode(object):
 
         # TODO: Subscribe to all the topics you need to
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_callback)
-        rospy.Subscriber('/current_velocity', TwistStamped, self.corrent_velocity_callback)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.current_velocity_callback)
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_callback)
 
         self.loop()
@@ -75,17 +76,18 @@ class DBWNode(object):
         self.controller.current_velocity = current_velocity
 
     def dbw_enabled_callback(self, dbw_enabled):
-        self.controller.enable() #Enabled always for the time being
+        self.controller.enable = True #dbw_enabled;
     
 
-    def loop(self):
+    def loop(self):s
         rate = rospy.Rate(50) # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            control.sample_time = 1/rate
-            throttle, brake, steering = self.controller.control(target_v = 10, target_angular_v=0, actual_v, dbw_status = True)
+            controller.sample_time = 1/rate
+            throttle, brake, steering = self.controller.control(10, 0, 0, True)
             #if <dbw is enabled>:
+            throttle = 0.5
             self.publish(throttle, brake, steer)
             rate.sleep()
 
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -8,18 +8,17 @@ from yaw_controller import YawController
 
 
 class Controller(object):
-    def __init__(self, vehicle_mass, wheel_radius, brake_deadband,
-                 decel_limit, accel_limit, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+    def __init__(self):
         # TODO: Implement
         # Fixed parameters
-        self.vehicle_mass = vehicle_mass
-        self.brake_deadband = brake_deadband
-        self.wheel_radius = wheel_radius
-        self.accel_limit = accel_limit
-        self.wheel_base = wheel_base
-        self.steer_ratio = steer_ratio
-        self.max_lat_accel = max_lat_accel
-        self.max_steer_angle = max_steer_angle
+        self.vehicle_mass = None
+        self.brake_deadband = None
+        self.wheel_radius = None
+        self.accel_limit = None
+        self.wheel_base = None
+        self.steer_ratio = None
+        self.max_lat_accel = None
+        self.max_steer_angle = None
         
         # To be Updated in each cycle
         self.twist_command = None
