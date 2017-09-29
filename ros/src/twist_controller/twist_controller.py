e894c61ddf87333778d5514207e5e4ac5d217db7
lazzybone <renlei1982@gmail.com>
2017-09-28 23:54:37 -0700

    Activate parameters for the yaw_controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,14 +12,16 @@ class Controller(object):
     def __init__(self):
         # TODO: Implement
         # Fixed parameters
-        self.vehicle_mass = 0
-        self.brake_deadband = 0
-        self.wheel_radius = 0
-        self.accel_limit = 0
-        self.wheel_base = 0
-        self.steer_ratio = 0
-        self.max_lat_accel = 0
-        self.max_steer_angle = 0
+        self.vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
+        self.fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
+        self.brake_deadband = rospy.get_param('~brake_deadband', .1)
+        self.decel_limit = rospy.get_param('~decel_limit', -5)
+        self.accel_limit = rospy.get_param('~accel_limit', 1.)
+        self.wheel_radius = rospy.get_param('~wheel_radius', 0.2413)
+        self.wheel_base = rospy.get_param('~wheel_base', 2.8498)
+        self.steer_ratio = rospy.get_param('~steer_ratio', 14.8)
+        self.max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
+        self.max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
 
         # To be Updated in each cycle
         self.twist_command = None
