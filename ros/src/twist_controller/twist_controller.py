d56da9d16c89a6bf802b3e6dcd405b61369d8898
lazzybone <renlei1982@gmail.com>
2017-10-08 23:42:24 -0700

    steer pid (0.5, 0.000001, 10)

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -32,7 +32,7 @@ class Controller(object):
 
 
         self.speed_PID = PID(0.2, 0.01, 0.1) # Dummy values
-        self.steer_PID = PID(0.5, 0.0001, 10, mn = -1, mx = 1) # To be adjusted
+        self.steer_PID = PID(0.5, 0.000001, 10, mn = -1, mx = 1) # To be adjusted
 
 
         #initial control values	
