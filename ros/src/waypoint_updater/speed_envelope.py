475a783ef7007e01f4ff0451a59c7a0cc9470875
psnx <panyitamas@gmail.com>
2017-10-14 18:11:27 +0000

    First experimental speed profile creation to be tested

--- ros/src/waypoint_updater/speed_envelope.py
+++ ros/src/waypoint_updater/speed_envelope.py
@@ -2,7 +2,7 @@
 from styx_msgs.msg import Lane, Waypoint
 
 import math
-import tf
+
 import numpy as np
 
 class SpeedEnvelope(object):
@@ -13,7 +13,7 @@ class SpeedEnvelope(object):
         pass
 
     def get_envelope(car_wp_id, red_light_wp_id, actual_v):
-        self.speed_envelope = self.base_wps[car_wp_id : red_wp_id]
+        speed_envelope = self.base_wps[car_wp_id : red_wp_id]
         for wp in speed_envelope:
             s = self.distance(self.base_wps, car_wp_id, red_light_wp_id)-safety_distance
             v_mps = self.actual_v * 0.44704
@@ -28,7 +28,7 @@ class SpeedEnvelope(object):
 
     def distance(self, waypoints, wp1, wp2):
         dist = 0
-        dl = lambda a,b math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2))
+        dl = lambda a,b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2))
         for i in range (wp1, wp2+1):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
