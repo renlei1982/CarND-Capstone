b3d8a3ab56f45abd530bc5f0c4d31501001ef623
lazzybone <renlei1982@gmail.com>
2017-10-15 18:33:34 -0700

    change the brake cal  brake = - max(min(abs(speed_command), 5.0), 0.0) * (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY)/(self.wheel_radius * 4)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -107,8 +107,8 @@ class WaypointUpdater(object):
             vel = math.sqrt(2 * MAX_DECEL * dist) * 3.6
             if vel < 1.:
                 vel = 0.
-            # wp.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
-            wp.twist.twist.linear.x = min(vel, 0.0)
+            wp.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
+            # wp.twist.twist.linear.x = min(vel, 0.0)
         return waypoints
 
 
