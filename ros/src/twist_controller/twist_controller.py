939c521c2051dda54c9818b8ad10533c0d59a977
lazzybone <renlei1982@gmail.com>
2017-10-15 19:34:33 -0700

    brake = -1000000000

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -58,7 +58,7 @@ class Controller(object):
             brake = 0.0
         elif speed_command < 0.0:
             throttle = speed_command
-            brake = 10000000000.0
+            brake = -10000000000.0
         return throttle, brake
 
     def control(self, target_v, yaw_angle, actual_v, cte_value, dbw_status):
